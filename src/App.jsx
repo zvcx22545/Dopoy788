4ab3aa73cf0bcf7d2db8655cf59827d56c1338fe
DRARINS <zvcx1236@gmail.com>
2024-02-16 23:30:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import MainLayout from "./components/Mainlayout/MainLayout"
+
+export default function App() {
+  return (
+  <MainLayout/>
+  )
+}
---
+++ src/components/Mainlayout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar';
+
+function MainLayout() {
+  return (
+    <section id="main-layout">
+    <Navbar></Navbar>
+    </section>
+  );
+}
+
+export default MainLayout
