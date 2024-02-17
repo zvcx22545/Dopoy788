e0080987f58e5e091368e9c8fc104628da7b1e47
Ado13e <77327642+properza@users.noreply.github.com>
2024-02-18 00:57:58 +0700

    src/components/Mainlayout/MainLayout.jsx

--- src/components/Mainlayout/MainLayout.jsx
+++ src/components/Mainlayout/MainLayout.jsx
@@ -1,5 +1,7 @@
 import Navbar from '../Navbar/Navbar';
+import Navlogin from '../Navlogin/Navlogin'
 import Tab from '../Tab/Tab';
+import Footer from '../Footer/Footer';
 
 function MainLayout() {
   return (
