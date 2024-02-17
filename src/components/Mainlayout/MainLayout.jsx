368cd23dcdbed07163b86f03dff0eaff6f876894
Alphax13 <nuttaya.qx@gmail.com>
2024-02-18 00:38:54 +0700

    Eve update

--- src/components/Mainlayout/MainLayout.jsx
+++ src/components/Mainlayout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar';
 import Tab from '../Tab/Tab';
+import BasicCard from '../card/card';
 
 function MainLayout() {
   return (
     <section id="main-layout">
     <Navbar></Navbar>
     <Tab/>
+    <BasicCard/>
     </section>
   );
 }
