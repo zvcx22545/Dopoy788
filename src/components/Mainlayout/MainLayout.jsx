96eeaa593bb0427feca00f8bc600c5aa6969e07f
DRARINS <zvcx1236@gmail.com>
2024-02-18 01:03:28 +0700

    src/components/Mainlayout/MainLayout.jsx

--- src/components/Mainlayout/MainLayout.jsx
+++ src/components/Mainlayout/MainLayout.jsx
@@ -1,5 +1,7 @@
 import Navbar from '../Navbar/Navbar';
 import Tab from '../Tab/Tab';
+import Navlogin from '../Navlogin/Navlogin'
+import Footer from "../Footer/Footer"
 
 function MainLayout() {
   return (
