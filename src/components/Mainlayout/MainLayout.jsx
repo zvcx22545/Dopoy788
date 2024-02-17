bc7db6dc1a5206d05d6288d5865a0bbe7fe053d8
Alphax13 <nuttaya.qx@gmail.com>
2024-02-18 00:48:52 +0700

    Update MainLayout.jsx

--- src/components/Mainlayout/MainLayout.jsx
+++ src/components/Mainlayout/MainLayout.jsx
@@ -1,5 +1,8 @@
 import Navbar from '../Navbar/Navbar';
 import Tab from '../Tab/Tab';
+import Navlogin from '../Navlogin/Navlogin';
+import ImgMediaCard from '../card/card';
+import Footer from '../Footer/Footer';
 
 function MainLayout() {
   return (
@@ -7,6 +10,9 @@ function MainLayout() {
     <Navbar></Navbar>
     <Navlogin/>
     <Tab/>
+    <ImgMediaCard/>
+    <Footer/>
+
     </section>
   );
 }
