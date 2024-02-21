d9f359da743f219a34086b26b8e5379b805f2638
DRARINS <zvcx1236@gmail.com>
2024-02-21 21:38:50 +0700

    src/components/Play/play.jsx

--- src/components/Play/play.jsx
+++ src/components/Play/play.jsx
@@ -14,8 +14,14 @@ function Play() {
   const [countdown, setCountdown] = useState(0);
   const [numberOfDigits, setNumberOfDigits] = useState(4);
   const [completedNumbers, setCompletedNumbers] = useState([]);
+  const [isReverseChecked, setIsReverseChecked] = useState(false); // State for checkbox
   const addCompletedNumbers = (newNumbers) => {
-    setCompletedNumbers([...completedNumbers, newNumbers]);
+    if (isReverseChecked) {
+      const reversedNumbers = reverseNumbers(newNumbers);
+      setCompletedNumbers([...completedNumbers, ...reversedNumbers]);
+    } else {
+      setCompletedNumbers([...completedNumbers, newNumbers]);
+    }
   };
 
   //   add active class for tab
@@ -41,6 +47,21 @@ function Play() {
         setActiveButtons([...activeButtons, buttonName]); // Add active state
       }
     }
+  };
+  
+
+  const handleCheckboxChange = () => {
+    setIsReverseChecked(!isReverseChecked); // Toggle the value
+  };
+
+  const reverseNumbers = (number) => {
+    const reversedNumbers = [];
+    const strNumber = number.toString();
+    for (let i = 0; i < strNumber.length; i++) {
+      const reversedNumber = strNumber.substring(i) + strNumber.substring(0, i);
+      reversedNumbers.push(reversedNumber);
+    }
+    return reversedNumbers;
   };
 
 
@@ -283,7 +304,7 @@ function Play() {
                     
                     <div className="left-content flex flex-col justify-center items-center">
                     <label className ="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" className ="sr-only peer" />
+                    <input type="checkbox" value="" className ="sr-only peer"  checked={isReverseChecked} onChange={handleCheckboxChange}/>
                     <div className ="relative w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all delay-100 dark:border-gray-600 peer-checked:bg-[#4400A5]"></div>
                     <div className ="text-[#4400A5] ms-3 text-[18.75px] font-medium dark:text-gray-300">กลับเลข</div>
                   </label>
