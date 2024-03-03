91c0db3b8be59645a848c2c85cb00e55fb73253b
DRARINS <zvcx1236@gmail.com>
2024-03-03 21:39:51 +0700

    src/components/card/card.jsx

--- src/components/ParentComponent/ParentComponent.jsx
+++ src/components/ParentComponent/ParentComponent.jsx
@@ -8,7 +8,7 @@ function ParentComponent() {
     Huaythais: "https://kosinstudio.com/wp-content/uploads/2018/07/02_Logo-Thailand-National-Football-Team-1932-2002.jpg",
     HuayLao: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Flag_of_Laos.svg/250px-Flag_of_Laos.svg.png",
     Huayhanoi: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/225px-Flag_of_Vietnam.svg.png",
-    Huayyiki: "https://mhandee888.com/wp-content/uploads/2022/06/%E0%B8%A2%E0%B8%B5%E0%B9%88%E0%B8%81%E0%B8%B5.png"
+    Huayyiki: "https://fast88.club/lotto-vi-logo.png"
   };
 
 
