0b0385429b38558e4fd798d73539ba468f61ef5c
Alphax13 <nuttaya.qx@gmail.com>
2024-03-03 18:27:36 +0700

    Update card.jsx

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import './card.css';
 import { Link } from 'react-router-dom'
 import { FaRegClock } from "react-icons/fa";
 
 export default function Card({ displayText }) {
+    const [countdown, setCountdown] = useState(0);
+    const [countdown2, setCountdown2] = useState(0);
+    useEffect(() => {
+        const countdownDate = new Date(Date.now() + 500 * 60 * 1000); // 2 minutes from now
+        const countdownDate2 = new Date(Date.now() + 350 * 60 * 1000); // 2 minutes from now
+
+        const interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+            const hours = Math.floor(
+                (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            );
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            setCountdown(`${hours}:${minutes}:${seconds}`);
+            if (distance < 0) {
+                clearInterval(interval);
+                setCountdown("EXPIRED");
+            }
+        }, 1000);
+
+        const interval2 = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate2 - now;
+            const hours = Math.floor(
+                (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            );
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            setCountdown2(`${hours}:${minutes}:${seconds}`);
+            if (distance < 0) {
+                clearInterval(interval2);
+                setCountdown2("EXPIRED");
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+            clearInterval(interval2);
+        };
+    }, []);
   return (
     <div className="card ">
         <div className="grid grid-cols-4 ">
@@ -23,12 +64,10 @@ export default function Card({ displayText }) {
             </div>
             
             <div className="bg-[#4400A5] rounded-lg text-white flex items-center justify-center p-3 mb-4">
-                <FaRegClock size={25} />
-                     <span className="countdown text-white  text-2xl text-center">
-                        <span style={{"--value":10}}></span>:
-                        <span style={{"--value":24}}></span>:
-                        <span style={{"--value":54}}></span>
-                    </span>
+            <FaRegClock size={25} />
+                <span className="countdown text-white text-2xl text-center">
+                    {countdown}
+                </span>
             </div>
             <Link to="/play"><button className="bg-[#FF8329] text-white text-2xl w-full py-2 rounded-lg">ใส่เลขแทง</button></Link>
             </div>
@@ -48,12 +87,10 @@ export default function Card({ displayText }) {
             </div>
             
             <div className="bg-[#4400A5] rounded-lg text-white flex items-center justify-center p-3 mb-4">
-                <FaRegClock size={25} />
-                     <span className="countdown text-white  text-2xl text-center">
-                        <span style={{"--value":10}}></span>:
-                        <span style={{"--value":24}}></span>:
-                        <span style={{"--value":54}}></span>
-                    </span>
+            <FaRegClock size={25} />
+                <span className="countdown text-white text-2xl text-center">
+                    {countdown2}
+                </span>
             </div>
             <button className="bg-[#FF8329] text-white text-2xl w-full py-2 rounded-lg">ใส่เลขแทง</button>
             </div>
@@ -73,12 +110,10 @@ export default function Card({ displayText }) {
             </div>
             
             <div className="bg-[#4400A5] rounded-lg text-white flex items-center justify-center p-3 mb-4">
-                <FaRegClock size={25} />
-                     <span className="countdown text-white  text-2xl text-center">
-                        <span style={{"--value":10}}></span>:
-                        <span style={{"--value":24}}></span>:
-                        <span style={{"--value":54}}></span>
-                    </span>
+            <FaRegClock size={25} />
+                <span className="countdown text-white text-2xl text-center">
+                    {countdown}
+                </span>
             </div>
                 <button className="bg-[#FF8329] text-white text-2xl w-full py-2 rounded-lg">ใส่เลขแทง</button>
             </div>
@@ -97,13 +132,12 @@ export default function Card({ displayText }) {
                 <h1 className="text-xl">เวลา : 14.30 น.</h1>
             </div>
             
+
             <div className="bg-[#4400A5] rounded-lg text-white flex items-center justify-center p-3 mb-4">
-                <FaRegClock size={25} />
-                     <span className="countdown text-white  text-2xl text-center">
-                        <span style={{"--value":10}}></span>:
-                        <span style={{"--value":24}}></span>:
-                        <span style={{"--value":54}}></span>
-                    </span>
+            <FaRegClock size={25} />
+                <span className="countdown text-white text-2xl text-center">
+                    {countdown2}
+                </span>
             </div>
                 <button className="bg-[#FF8329] text-white text-2xl w-full py-2 rounded-lg">ใส่เลขแทง</button>
             </div>
