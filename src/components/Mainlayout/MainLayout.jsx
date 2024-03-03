5fd342b52651096de878a4f9e873a531bd280f95
DRARINS <zvcx1236@gmail.com>
2024-03-03 17:46:01 +0700

    src/components/Navbar/Navbar.jsx

--- src/components/Mainlayout/MainLayout.jsx
+++ src/components/Mainlayout/MainLayout.jsx
@@ -1,5 +1,4 @@
 import Navbar from '../Navbar/Navbar';
-import Navlogin from '../Navlogin/Navlogin'
 // import Tab from '../Tab/Tab';
 import Footer from '../Footer/Footer';
 // import Card from '../card/card';
@@ -7,19 +6,19 @@ import Rule from '../Rule/RuleBtn';
 import Resultrotte from '../Resultrotte/Resultroote';
 import Redeem from '../Redeem/Redeem';
 import ParentComponent from '../ParentComponent/ParentComponent';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+// import { useSelector } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
 
 function MainLayout() {
-  const navigate = useNavigate();
-  const userToken = useSelector((state) => state.user.userToken);
-  useEffect(() => {
-    // Check if the user is logged in
-    if (!userToken) {
-      navigate('/Login'); // Redirect to login page if not logged in
-    }
-  }, [userToken, navigate]);
+  // const navigate = useNavigate();
+  // const userToken = useSelector((state) => state.user.userToken);
+  // useEffect(() => {
+  //   // Check if the user is logged in
+  //   if (!userToken) {
+  //     navigate('/Login'); // Redirect to login page if not logged in
+  //   }
+  // }, [userToken, navigate]);
   return (
     <section id="main-layout">
       <Navbar/>
